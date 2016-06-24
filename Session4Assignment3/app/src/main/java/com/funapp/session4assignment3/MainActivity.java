8a465ae81df1c708d148c4c97e2f66e1aee2a958
kkellyin90 <keith.kelly.a@gmail.com>
2016-06-24 21:40:52 +0530

    gridView

---
+++ Session4Assignment3/app/src/main/java/com/funapp/session4assignment3/MainActivity.java
@@ -0,0 +1,28 @@
+package com.funapp.session4assignment3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, +position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
